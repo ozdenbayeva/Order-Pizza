5bb6ec014479399fc04f74a5a69cb559f75d5024
vipMura <agabyl.mura@gmail.com>
2015-03-07 23:01:59 +0600

    Created class map that shows Almaty

--- app/src/main/java/homeorderproject/mura/kz/edu/sdu/homeorderproject/map.java
+++ app/src/main/java/homeorderproject/mura/kz/edu/sdu/homeorderproject/map.java
@@ -1,7 +1,18 @@
 package homeorderproject.mura.kz.edu.sdu.homeorderproject;
 
 import android.app.Activity;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
 /**
@@ -9,11 +20,31 @@ import android.os.Bundle;
  */
 public class map extends Activity{
 
+    static final LatLng Almaty = new LatLng(43.2565400, 76.9284800);
+    private GoogleMap map;
+    private double latitude;
+    private Location location;
+    private LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.map);
+        locationManager = (LocationManager) this
+                .getSystemService(LOCATION_SERVICE);
+        location = locationManager
+                .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        latitude = location.getLatitude();
 
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+
+
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(Almaty, 50));
+
+        // Zoom in, animating the camera.
+        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
+        Log.d("asd",map.getMyLocation() + " ");
 
     }
 

ad0957fb845849befd71c0163ff003ae01a63b03
vipMura <agabyl.mura@gmail.com>
2015-03-11 00:51:31 +0600

    Added geocoder, that finds you place and alerts if it's not Almaty

--- app/src/main/java/homeorderproject/mura/kz/edu/sdu/homeorderproject/map.java
+++ app/src/main/java/homeorderproject/mura/kz/edu/sdu/homeorderproject/map.java
@@ -2,9 +2,12 @@ package homeorderproject.mura.kz.edu.sdu.homeorderproject;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Message;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +24,10 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 
 /**
@@ -34,7 +42,11 @@ public class map extends Activity{
     private Location location;
     private ImageButton gps;
     private Marker marker;
+    private Geocoder geocoder;
     private LocationManager locationManager;
+    private List<Address> list;
+    private Address address;
+    private String result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -78,6 +90,8 @@ public class map extends Activity{
                 map.clear();
                 marker = map.addMarker(new MarkerOptions().position(Almaty).title("Новое местоположение"));
 
+                geoCode();
+
 
 
             }
@@ -86,6 +100,57 @@ public class map extends Activity{
 
 
     }
+
+    private void geoCode(){
+        Thread thread = new Thread() {
+            @Override public void run() {
+                geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+                result = "";
+                try {
+                    list = geocoder.getFromLocation(
+                            latitude, longtitude, 1);
+                    if (list != null && list.size() > 0) {
+                        address = list.get(0);
+                        // sending back first address line and locality
+                        if (address.getAddressLine(1).equals("Алматы")){
+                            result = address.getAddressLine(0) +
+                                    ", " + address.getLocality() +
+                                    ", " + address.getAddressLine(1) +
+                                    ", " + address.getAddressLine(2);
+                        }
+                        else{
+                            result = "0";
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+
+                                    Toast.makeText(getApplicationContext(),
+                                            "Извините, но только Алматы",Toast.LENGTH_LONG).show();
+                                    map.clear();
+                                }
+                            });
+
+                        }
+
+                    }
+                } catch (IOException e) {
+                    Log.e("asd", "Impossible to connect to Geocoder", e);
+                } finally {
+                    Message msg = Message.obtain();
+                    if (result != null) {
+                        msg.what = 1;
+                        Bundle bundle = new Bundle();
+                        bundle.putString("address", result);
+                        msg.setData(bundle);
+                    } else
+                        msg.what = 0;
+                    Log.d("asdasd", result);
+                    Log.d("asdasd", latitude + " " + longtitude);
+                }
+            }
+        };
+        thread.start();
+    }
+
 
     private void initialize(){
         Almaty  = new LatLng(43.2565400, 76.9284800);
